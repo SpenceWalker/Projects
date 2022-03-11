2e1a5e18a821502c25d00ae477279baa3234ec34
Spencer Walker <walkersl0209@gmail.com>
2022-03-10 23:15:13 -0500

    Capstone is affecting my mental health

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/UserService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/UserService.java
@@ -1,6 +1,8 @@
 package com.techelevator.tenmo.services;
 
 import com.techelevator.tenmo.model.User;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 public class UserService {
@@ -9,6 +11,18 @@ public class UserService {
     private final RestTemplate restTemplate = new RestTemplate();
 
 
-    public User
+    public User[] getListOfAllUsers(){
 
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<User[]> entity = new HttpEntity<>(headers);
+
+
+        ResponseEntity<User[]> response = restTemplate.exchange(API_BASE_URL + "/users/", HttpMethod.GET, entity, User[].class);
+
+        return response.getBody();
+    }
 }
